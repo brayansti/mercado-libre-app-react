155099521c261f64554b6e5c23587d000eb6c060
Brayan Stiven Camargo Gaitan <brayan.camargo@globant.com>
2021-06-16 23:11:54 -0500

    Add router and sass

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {BrowserRouter as Router, Link, useLocation, Route, Switch} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <SearchBar />
+
+        <Switch>
+          <Route exact path="/">
+            Welcome
+          </Route>
+          <Route path="/detail">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe iste consequatur sunt! Quas doloribus, excepturi tempore nemo numquam porro. Vitae minus perferendis deserunt cumque provident voluptatibus ipsum blanditiis quo odit!
+          </Route>
+        </Switch>
+
+      </Router>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +32,55 @@ function App() {
           Learn React
         </a>
       </header>
+    </div>
+  );
+}
+
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+
+
+function SearchBar() {
+  let query = useQuery();
+
+  return (
+    <div>
+      <div>
+        <h2>details</h2>
+        <ul>
+          <li>
+            <Link to="/detail?name=netflix">Netflix</Link>
+          </li>
+          <li>
+            <Link to="/detail?name=zillow-group">Zillow Group</Link>
+          </li>
+          <li>
+            <Link to="/detail?name=yahoo">Yahoo</Link>
+          </li>
+          <li>
+            <Link to="/detail?name=modus-create">Modus Create</Link>
+          </li>
+        </ul>
+
+        <Child name={query.get("name")} />
+      </div>
+    </div>
+  );
+}
+
+
+function Child({ name }) {
+  return (
+    <div>
+      {name ? (
+        <h3>
+          The <code>name</code> in the query string is &quot;{name}
+          &quot;
+        </h3>
+      ) : (
+        <h3>There is no name in the query string</h3>
+      )}
     </div>
   );
 }
